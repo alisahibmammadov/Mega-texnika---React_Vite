3a865e2bc08a8a9e6db55ec45216e7c078345b97
alisahibmammadov <ali.yek.10@mail.ru>
2023-10-06 23:36:07 +0400

    project first configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Header from "./layout/Header";
+import Navbar from "./layout/Navbar";
+import Footer from "./layout/Footer";
 
 function App() {
-
   return (
-   <div></div>
-  )
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
